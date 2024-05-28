2e0d27912b20a7812c78e42378a9dc2eb1947ef8
Ebasosinsa <nordic123@mail.ru>
2024-05-28 16:41:25 +0300

    Toolkit page Add

--- client/src/app/components/toolkit/toolkit.component.ts
+++ client/src/app/components/toolkit/toolkit.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+interface mockToolKit {
+  id: number;
+}
+
 @Component({
   selector: 'app-toolkit',
   templateUrl: './toolkit.component.html',
-  styleUrl: './toolkit.component.scss'
+  styleUrl: './toolkit.component.scss',
 })
 export class ToolkitComponent {
-
+  mockToolKits: mockToolKit[] = [
+    {
+      id: 1,
+    },
+    {
+      id: 2,
+    },
+    {
+      id: 3,
+    },
+    {
+      id: 4,
+    },
+    {
+      id: 5,
+    },
+    {
+      id: 6,
+    },
+    {
+      id: 7,
+    },
+    {
+      id: 8,
+    },
+    {
+      id: 9,
+    },
+    {
+      id: 10,
+    },
+  ];
 }
